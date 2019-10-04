710c49fcf970a2795a5d98a60f115a0e16f6c5c4
Nawaf ALrifai <nbalrifai@gmail.com>
2019-10-04 15:33:33 +0300

    auth

---
+++ rejoy/forms/Registration.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import AuthStore from "../stores/AuthStore";
+
+class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: ""
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    AuthStore.signupUser(this.state);
+  };
+
+  render() {
+    const { username, email, password, first_name, last_name } = this.state;
+
+    return (
+      <div className="col-6 mx-auto">
+        <div className="card my-5">
+          <div className="card-body">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  value={username}
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  value={password}
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="first_name">First Name</label>
+                <input
+                  type="first_name"
+                  className="form-control"
+                  id="first_name"
+                  value={first_name}
+                  name="first_name"
+                  placeholder="first_name"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="last_name">Last Name</label>
+                <input
+                  type="last_name"
+                  className="form-control"
+                  id="last_name"
+                  value={last_name}
+                  name="last_name"
+                  placeholder="last_name"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Signup
+              </button>
+              <Link to="/login" className="btn btn-link my-2 my-sm-0">
+                I already have an account
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
---
+++ rejoy/src/stores/authStore.js
@@ -0,0 +1,59 @@
+import { decorate, observable, computed } from "mobx";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
+export const instance = axios.create({
+  baseURL: "https://http://127.0.0.1:8000/.com/"
+});
+
+class AuthStore {
+  user = null;
+
+  setUser = token => {
+    if (token) {
+      axios.defaults.headers.common.Authorization = `JWT ${token}`;
+      const decodedUser = jwt_decode(token);
+      this.user = decodedUser;
+    } else {
+      delete axios.defaults.headers.common.Authorization;
+      this.user = null;
+    }
+  };
+
+  signupUser = async userData => {
+    try {
+      await instance.post("register/", userData);
+      this.login(userData);
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  loginUser = async userData => {
+    try {
+      const res = await instance.post("login/", userData);
+      const user = res.data;
+      this.setUser(user.token);
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  logout = () => {
+    this.setUser();
+  };
+}
+
+decorate(AuthStore, {
+  // username: observable,
+  // email: observable,
+  // // password: observable,
+  // The above is already being accessed/retrieved from the user
+  user: observable
+});
+
+const authStore = new AuthStore();
+// remember that you are creating a variable to export it, as you are not export the
+// class itself but the object that is being export
+
+export default authStore;
